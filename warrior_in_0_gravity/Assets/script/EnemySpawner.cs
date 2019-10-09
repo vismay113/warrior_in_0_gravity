bb0b9e5400f88d710fb4c1ecab8de077890d19e4
vismay2711 <visu.patel11@gmail.com>
2019-10-09 11:09:16 +0530

    multiple enemy spawning mechanism is created

---
+++ warrior_in_0_gravity/Assets/script/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // configuration variables
+    [SerializeField] List<waveConfig> waveList;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveList[startingWave];
+
+        StartCoroutine(SpawAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawAllEnemiesInWave (waveConfig waveConfig)
+    {
+        for (int enemyIndex = 0; enemyIndex < waveConfig.GetNumberOfEnemies(); enemyIndex++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetPathWayPoints()[0].transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ warrior_in_0_gravity/Assets/script/enemyPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPath : MonoBehaviour
+{
+    // configuration variables
+    [SerializeField] waveConfig waveConf;
+    List<Transform> enemyWayPonits;
+    [SerializeField] float moveSpeed = 2f;
+    int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyWayPonits = waveConf.GetPathWayPoints();
+        transform.position = enemyWayPonits[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveEnemy();
+    }
+
+    private void moveEnemy()
+    {
+        if (wayPointIndex <= enemyWayPonits.Count - 1)
+        {
+            var targetPosition = enemyWayPonits[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
